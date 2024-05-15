c8a203c0b64a0bc1538ae370c059f35c39778c2d
BrianDuvall0 <brianduvall34@gmail.com>
2024-05-15 19:05:43 -0300

    Prototipo pagina responsive web

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import Anticonceptivos from './pages/Anticonceptivos'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Anticonceptivos/>
+  </React.StrictMode>,
+)
